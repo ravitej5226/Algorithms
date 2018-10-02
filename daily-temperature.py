ab3f3a34fe744bfe10c92e2a9b1b1da0611671e4
Raghav <ravitej5226@gmail.com>
2018-10-02 14:57:18 -0700

    Updated algorithm to run in O(n)

--- daily-temperature.py
+++ daily-temperature.py
@@ -5,42 +5,34 @@
 # Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
 
 class Solution(object):
-    temperature_stack=[]
-    temperature_dict={}
+      
     def dailyTemperatures(self, temperatures):
         """
         :type temperatures: List[int]
         :rtype: List[int]
         """
         result=[0]*len(temperatures)        
-        for index,current_temp in enumerate(temperatures):            
-            if(len(self.temperature_stack)==0):
-                self.push(index)
-                self.temperature_dict[index]=current_temp
-            else:
-                while(len(self.temperature_stack)>0):
-                    popped_element=self.pop()
-                    if(current_temp>self.temperature_dict[popped_element]):
-                        result[popped_element]=index-popped_element
-                    else:
-                        self.push(popped_element)                                                
-                        break;
-                self.push(index)
-                self.temperature_dict[index]=current_temp
-         
+        temperature_stack=[]  
+        for index in xrange(len(temperatures)-1,-1,-1):            
+            current_temperature=temperatures[index]
+            if(len(temperature_stack)==0):
+                temperature_stack.append(index)
+            else:                
+                popped_index=temperature_stack.pop()
+                popped_temperature=temperatures[popped_index]
+                while(current_temperature>=popped_temperature and len(temperature_stack)>0):
+                    popped_index=temperature_stack.pop()
+                    popped_temperature=temperatures[popped_index]
+                
+                if(popped_temperature>current_temperature):
+                    result[index]=popped_index-index
+                    temperature_stack.append(popped_index)                    
+                else:
+                    result[index]=0            
+                temperature_stack.append(index)
+        
         return result                   
 
     
-    def push(self,temperature):
-        self.temperature_stack.append(temperature)
-    
-    def pop(self):                
-        pop_element=self.temperature_stack[-1]
-        self.temperature_stack=self.temperature_stack[0:-1]
-        return pop_element
-        
-    
-
 s=Solution();
-print(s.dailyTemperatures([73,74,75,71,69,72,76,73]))
-#print(s.dailyTemperatures([76]*30000))
+print(s.dailyTemperatures([89,62,70,58,47,47,46,76,100,70]))
